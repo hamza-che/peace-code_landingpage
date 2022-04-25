5e3eddcf98d203988ef7b47386b4bf94527a5428
hamza-che <chenguitiha@gmail.com>
2022-04-25 17:01:49 +0100

    event count down funtionality added

--- js/countDown.js
+++ js/countDown.js
@@ -1,30 +1,10 @@
-const statsSection = document.getElementById("statistics");
-const statNumbers = document.querySelectorAll("#statistics .stat .number");
-let functionStarted = false;
-
-window.onscroll = function() {
-  if (window.scrollY >= statsSection.offsetTop) {
-    !functionStarted && statNumbers.forEach(number => startCounting(number));
-    functionStarted = true;
-  }
-};
-
-function startCounting(element) {
-  let goal = element.dataset.goal;
-  let count = setInterval(() => {
-    element.textContent++;
-    element.textContent == goal && clearInterval(count);
-  }, 1500 / goal);
-}
-
-// Event count down
 const daysContainer = document.querySelector(".latest-events .days");
 const hoursContainer = document.querySelector(".latest-events .hours");
 const minutesContainer = document.querySelector(".latest-events .minutes");
 const secondsContainer = document.querySelector(".latest-events .seconds");
 const countDownDate = new Date("Dec 31, 2022 23:59:59").getTime();
 
-const counter = setInterval(() => {
+function countDown() {
   const nowDate = new Date().getTime();
   const diffDate = countDownDate - nowDate;
 
@@ -45,6 +25,6 @@ const counter = setInterval(() => {
   seconds >= 10
     ? (secondsContainer.innerHTML = seconds)
     : (secondsContainer.innerHTML = `0${seconds}`);
-}, 1000);
+}
 
-console.log(typeof count);
+export default countDown;
